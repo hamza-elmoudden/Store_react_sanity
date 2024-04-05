7eee99808ed85168d57aa8fb7e92bd85604b6c28
hamza-elmoudden <hamzaelmouddane2000@gmail.com>
2024-04-05 09:38:12 +0000

    Store React app use Sanity to controle data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { Home } from './pages/Home';
+import { Routes,Route } from "react-router-dom";
+import { Proudate } from './pages/Proudate';
+import { Order } from './pages/Order';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/proudate' element={<Proudate/>}/>
+      <Route path='/Order/:idprd' element={<Order/>}/>
+      <Route path='/Order' element={<Order/>}/>
+
+    </Routes>
+    </>
   );
 }
 
